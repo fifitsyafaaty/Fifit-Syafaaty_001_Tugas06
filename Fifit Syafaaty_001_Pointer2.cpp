22ab582fed071dd77a4e98685c8ecb4899b46441
fifitsyafaaty <90440660+fifitsyafaaty@users.noreply.github.com>
2021-11-19 19:44:40 +0700

    Add files via upload

---
+++ Fifit Syafaaty_001_Pointer2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+ 
+int main () {
+int a = 0; //banyaknya element data
+int i; //untuk iterasi for
+int sum = 0, max = 0, min = 0;
+ 
+//minta user input banyaknya data
+cout << "Masukkan banyak data nilai :";
+cin >> a;
+ 
+//bikin array dengan panjang a
+int data[a];
+ 
+//minta user input untuk tiap-tiap element
+for(i=0; i<a; i++) {
+cout << "Nilai ke ";
+cout << i+1;
+cout << " : ";
+cin >> data[i];
+}
+//Pake pointer
+sum = 0;
+int *ptr;
+ptr = data;
+ 
+cout << "\n Pake Pointer :";
+cout << "\n Data nilai yang anda masukan adalah "; // hapus baris yang ini kalo nggag mau nampilin lagi elemennya
+max = min = *ptr;
+for(i=0; i<a; i++) {
+cout << *(ptr + i) << ","; // hapus baris yang ini kalo nggag mau nampilin lagi elemennya
+ 
+sum = sum + *(ptr + i);
+if (max < *(ptr + i)) {
+max = *(ptr + i);
+}
+if (min > *(ptr + i)) {
+min = *(ptr + i);
+}
+}
+cout << "\n Jumlah data nilai   : " << sum;
+cout << "\n Rata-rata nilai     : " << sum/a;
+cout << "\n Nilai Minimal       : " << min;
+cout << "\n Nilai Maksimal      : " << max;
+return 0;
+}
